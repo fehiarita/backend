bbfa57c3100433111a4b9455b768f21b7f31fd6d
Lucas <135342808+LucasStranger@users.noreply.github.com>
2024-04-25 14:00:56 -0300

    Update Tabelas em Sql.sql

--- Tabelas em Sql.sql
+++ Tabelas em Sql.sql
@@ -6,3 +6,30 @@ CREATE TABLE Historico_de_Jogos (
     resultado VARCHAR(10),
     detalhes TEXT
 );
+CREATE TABLE Estatisticas (
+    estatisticas_id INT PRIMARY KEY,
+    jogador_id INT,
+    partidas_jogadas INT,
+    vitorias INT,
+    empates INT,
+    derrotas INT,
+    gols_marcados INT,
+    gols_sofridos INT
+);
+CREATE TABLE Head_To_Head (
+    head_to_head_id INT PRIMARY KEY,
+    jogador1_id INT,
+    jogador2_id INT,
+    vitorias_jogador1 INT,
+    vitorias_jogador2 INT,
+    empates INT
+);
+CREATE TABLE meu_perfil (
+    perfil_id INT PRIMARY KEY,
+    usuario_id INT,
+    foto_perfil VARCHAR(255),
+    biografia TEXT,
+    localizacao VARCHAR(100),
+    data_nascimento DATE,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id)
+);
